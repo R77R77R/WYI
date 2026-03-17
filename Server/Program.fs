940b8c1094bc5f6574b2dd1ef4d01c1610721195
R <siduochen@hotmail.com>
2026-03-17 18:44:10 -0400

    s

--- Server/Program.fs
+++ Server/Program.fs
@@ -10,6 +10,21 @@ open UtilWebServer.Common
 [<EntryPoint>]
 let main argv =
 
+    TypeSys.CodeRobot.go 
+        output 
+        @"C:\Dev\JCS\TypeSys\bin\Debug\net10.0"
+        {
+            ns = "WYI.Shared"
+            rdbms = Util.Db.Rdbms.PostgreSql
+            dbName = "wyi"
+            donmainName = ""
+            conn = "server=127.0.0.1; user=sa; database=Studio"
+            mainDir = @"C:\Dev\WYI\WYI.Shared"
+            JsDir = @"C:\Dev\WYI\vscode\src\lib\shared" }
+    //JCS.BizLogics.Common.runtime.data.projectxs[234354L] 
+    //|> JCS.BizLogics.CodeRobot.runProject
+
+
     WYI.BizLogics.Launcher.launch()
 
     Util.Runtime.halt output "" ""
