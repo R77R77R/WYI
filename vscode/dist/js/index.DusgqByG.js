1291b4e8a1ee7180a37c46c4a1c4eff173c6b4cf
R <siduochen@hotmail.com>
2026-03-20 00:41:01 -0400

    auto-deploy

--- vscode/dist/js/index.DusgqByG.js
+++ vscode/dist/js/index.DusgqByG.js
@@ -12293,15 +12293,15 @@ const _hoisted_11$2 = {
   id: "book",
   class: "my-5"
 };
-const _hoisted_12$1 = /* @__PURE__ */ createBaseVNode("div", null, "Contact by email: info@sdchen.art", -1);
+const _hoisted_12$2 = /* @__PURE__ */ createBaseVNode("div", null, "Contact by email: info@sdchen.art", -1);
 const _hoisted_13$1 = { key: 0 };
 const _hoisted_14$1 = {
   key: 1,
   class: "m-3 p-3 bg-[#aabb99] w-[1000px]"
 };
-const _hoisted_15$2 = /* @__PURE__ */ createBaseVNode("div", null, "Name", -1);
+const _hoisted_15$1 = /* @__PURE__ */ createBaseVNode("div", null, "Name", -1);
 const _hoisted_16$2 = /* @__PURE__ */ createBaseVNode("div", null, "Email", -1);
-const _hoisted_17 = /* @__PURE__ */ createBaseVNode("div", null, "Message", -1);
+const _hoisted_17$1 = /* @__PURE__ */ createBaseVNode("div", null, "Message", -1);
 const _hoisted_18 = /* @__PURE__ */ createStaticVNode('<div id="painting" class="caption-1">Painting Progress</div><div class="lg:flex justify-start"><img class="painting" src="https://i.imgur.com/nMqz18N.jpeg"><img class="painting" src="https://i.imgur.com/ypjl9oY.jpeg"><img class="painting" src="https://i.imgur.com/at8cz1s.jpeg"></div><div class="lg:flex justify-end"><img class="painting" src="https://i.imgur.com/JaH5I2S.jpeg"><img class="painting" src="https://i.imgur.com/65JdVrh.jpeg"><img class="painting" src="https://i.imgur.com/uLdMbny.jpeg"></div><div class="lg:flex justify-start"><img class="painting" src="https://i.imgur.com/6bipccJ.jpeg"><img class="painting" src="https://i.imgur.com/TSwr3Pw.jpeg"><img class="painting" src="https://i.imgur.com/FygNW9w.jpeg"></div><div class="lg:flex justify-end"><img class="painting" src="https://i.imgur.com/mJkTKta.jpeg"><img class="painting" src="https://i.imgur.com/awNWKJf.jpeg"><img class="painting" src="https://i.imgur.com/BNeLCuO.jpeg"></div><div class="lg:flex justify-start"><img class="painting" src="https://i.imgur.com/YAW4jVN.jpeg"><img class="painting" src="https://i.imgur.com/ypiRqH5.jpeg"><img class="painting" src="https://i.imgur.com/fTlsPsu.jpeg"></div><div id="price" class="caption-1">Price</div><div> Typically a 10&quot; x 12&quot; portrait is done in two sessions. Each session lasts approx. 3 hours. There is an intermission between the sessions to allow the oil to dry. A portrait is $400 for the standard hourly rate of $65/hour plus the materials. </div><div id="lesson" class="caption-1">Lesson</div><div><a href="#book">Book</a> portrait oil painting lessons. <br>Master the art of oil portrait painting through private lessons. Available online or in-person. <br>Session length: 3 hours <br>Rate: $40/hour (plus tax) </div>', 10);
 const _hoisted_28 = /* @__PURE__ */ createBaseVNode("div", { class: "hor" }, null, -1);
 const _hoisted_29 = /* @__PURE__ */ createBaseVNode("div", { class: "flex justify-center" }, [
@@ -12343,13 +12343,13 @@ const _sfc_main$3 = /* @__PURE__ */ defineComponent({
           createBaseVNode("div", _hoisted_4$3, [
             _hoisted_5$3,
             createBaseVNode("div", _hoisted_11$2, [
-              _hoisted_12$1,
+              _hoisted_12$2,
               unref(s).expand == false ? (openBlock(), createElementBlock("div", _hoisted_13$1, [
                 createBaseVNode("button", {
                   onClick: _cache[0] || (_cache[0] = ($event) => unref(s).expand = true)
                 }, "Book Now")
               ])) : (openBlock(), createElementBlock("div", _hoisted_14$1, [
-                _hoisted_15$2,
+                _hoisted_15$1,
                 withDirectives(createBaseVNode("input", {
                   "onUpdate:modelValue": _cache[1] || (_cache[1] = ($event) => unref(s).name = $event),
                   type: "text",
@@ -12365,7 +12365,7 @@ const _sfc_main$3 = /* @__PURE__ */ defineComponent({
                 }, null, 512), [
                   [vModelText, unref(s).email]
                 ]),
-                _hoisted_17,
+                _hoisted_17$1,
                 withDirectives(createBaseVNode("textarea", {
                   "onUpdate:modelValue": _cache[3] || (_cache[3] = ($event) => unref(s).msg = $event),
                   class: "w-[800px] h-[500px]"
@@ -12398,10 +12398,10 @@ const _hoisted_8$2 = { class: "card" };
 const _hoisted_9$2 = /* @__PURE__ */ createBaseVNode("div", { class: "card-caption" }, "Log", -1);
 const _hoisted_10$2 = ["innerHTML"];
 const _hoisted_11$1 = ["innerHTML"];
-const _hoisted_12 = /* @__PURE__ */ createBaseVNode("hr", { class: "mt-3" }, null, -1);
+const _hoisted_12$1 = /* @__PURE__ */ createBaseVNode("hr", { class: "mt-3" }, null, -1);
 const _hoisted_13 = { class: "card" };
 const _hoisted_14 = /* @__PURE__ */ createBaseVNode("div", { class: "card-caption" }, "Page Log", -1);
-const _hoisted_15$1 = /* @__PURE__ */ createBaseVNode("hr", { class: "mt-3" }, null, -1);
+const _hoisted_15 = /* @__PURE__ */ createBaseVNode("hr", { class: "mt-3" }, null, -1);
 const _hoisted_16$1 = /* @__PURE__ */ createBaseVNode("div", { class: "flex justify-center" }, [
   /* @__PURE__ */ createBaseVNode("div", { class: "hor-range" })
 ], -1);
@@ -12455,7 +12455,7 @@ const _sfc_main$2 = /* @__PURE__ */ defineComponent({
                   createBaseVNode("div", {
                     innerHTML: log.p.Content
                   }, null, 8, _hoisted_11$1),
-                  _hoisted_12
+                  _hoisted_12$1
                 ]);
               }), 256))
             ]),
@@ -12467,7 +12467,7 @@ const _sfc_main$2 = /* @__PURE__ */ defineComponent({
                   createBaseVNode("div", null, toDisplayString(plog.ip), 1),
                   createBaseVNode("div", null, toDisplayString(plog.pathline), 1),
                   createBaseVNode("div", null, toDisplayString(plog.from), 1),
-                  _hoisted_15$1
+                  _hoisted_15
                 ]);
               }), 256))
             ])
@@ -13340,24 +13340,25 @@ const _sfc_main$1 = /* @__PURE__ */ defineComponent({
     };
   }
 });
-const _hoisted_1 = /* @__PURE__ */ createBaseVNode("div", null, "postgresql + F# + Kestrel + vue", -1);
-const _hoisted_2 = /* @__PURE__ */ createBaseVNode("div", null, "https://whatsyourideal.com/", -1);
-const _hoisted_3 = /* @__PURE__ */ createBaseVNode("div", null, "Lower Your Bills On CruiseControl!", -1);
-const _hoisted_4 = { class: "lg:w-[1200px] mr-[10px]" };
-const _hoisted_5 = { class: "main-color w-screen" };
-const _hoisted_6 = { class: "flex justify-start" };
-const _hoisted_7 = /* @__PURE__ */ createBaseVNode("div", { class: "w-full flex justify-center bg-[#262622]" }, [
+const _hoisted_1 = /* @__PURE__ */ createBaseVNode("div", null, "Auto remote deploy", -1);
+const _hoisted_2 = /* @__PURE__ */ createBaseVNode("div", null, "postgresql + F# + Kestrel + vue", -1);
+const _hoisted_3 = /* @__PURE__ */ createBaseVNode("div", null, "https://whatsyourideal.com/", -1);
+const _hoisted_4 = /* @__PURE__ */ createBaseVNode("div", null, "Lower Your Bills On CruiseControl!", -1);
+const _hoisted_5 = { class: "lg:w-[1200px] mr-[10px]" };
+const _hoisted_6 = { class: "main-color w-screen" };
+const _hoisted_7 = { class: "flex justify-start" };
+const _hoisted_8 = /* @__PURE__ */ createBaseVNode("div", { class: "w-full flex justify-center bg-[#262622]" }, [
   /* @__PURE__ */ createBaseVNode("img", {
     class: "w-[300px] my-3",
     src: "https://i.imgur.com/JvdrjQP.png"
   })
 ], -1);
-const _hoisted_8 = { class: "flex justify-center p-3" };
-const _hoisted_9 = { class: "flex hor-range" };
-const _hoisted_10 = { class: "menu" };
-const _hoisted_11 = /* @__PURE__ */ createStaticVNode('<div class="menu"><a href="#painting">Painting</a></div><div class="menu"><a href="#price">Price</a></div><div class="menu"><a href="#lesson">Lesson</a></div><div class="bg-white"><a href="https://www.yelp.com/biz/sdcheen-fine-art-studio-dingmans-ferry" target="_blank"><img src="https://s3-media0.fl.yelpcdn.com/assets/srv0/yelp_design_cdn/7ef71bf77a33/assets/img/brand/logo_desktop.svg"></a></div>', 4);
-const _hoisted_15 = /* @__PURE__ */ createBaseVNode("div", { class: "hor" }, null, -1);
-const _hoisted_16 = /* @__PURE__ */ createBaseVNode("div", { class: "flex justify-center" }, [
+const _hoisted_9 = { class: "flex justify-center p-3" };
+const _hoisted_10 = { class: "flex hor-range" };
+const _hoisted_11 = { class: "menu" };
+const _hoisted_12 = /* @__PURE__ */ createStaticVNode('<div class="menu"><a href="#painting">Painting</a></div><div class="menu"><a href="#price">Price</a></div><div class="menu"><a href="#lesson">Lesson</a></div><div class="bg-white"><a href="https://www.yelp.com/biz/sdcheen-fine-art-studio-dingmans-ferry" target="_blank"><img src="https://s3-media0.fl.yelpcdn.com/assets/srv0/yelp_design_cdn/7ef71bf77a33/assets/img/brand/logo_desktop.svg"></a></div>', 4);
+const _hoisted_16 = /* @__PURE__ */ createBaseVNode("div", { class: "hor" }, null, -1);
+const _hoisted_17 = /* @__PURE__ */ createBaseVNode("div", { class: "flex justify-center" }, [
   /* @__PURE__ */ createBaseVNode("div", { class: "hor-range" }, [
     /* @__PURE__ */ createBaseVNode("div", { class: "pt-2 pb-5" }, [
       /* @__PURE__ */ createBaseVNode("br"),
@@ -13375,15 +13376,16 @@ const _sfc_main = {
         _hoisted_1,
         _hoisted_2,
         _hoisted_3,
-        createBaseVNode("div", _hoisted_4, [
-          createBaseVNode("div", _hoisted_5, [
-            createBaseVNode("div", _hoisted_6, [
+        _hoisted_4,
+        createBaseVNode("div", _hoisted_5, [
+          createBaseVNode("div", _hoisted_6, [
+            createBaseVNode("div", _hoisted_7, [
               createVNode(_sfc_main$1, { onChanged: _ctx.onAuthChanged }, null, 8, ["onChanged"])
             ]),
-            _hoisted_7,
-            createBaseVNode("div", _hoisted_8, [
-              createBaseVNode("div", _hoisted_9, [
-                createBaseVNode("div", _hoisted_10, [
+            _hoisted_8,
+            createBaseVNode("div", _hoisted_9, [
+              createBaseVNode("div", _hoisted_10, [
+                createBaseVNode("div", _hoisted_11, [
                   createVNode(_component_router_link, { to: "/" }, {
                     default: withCtx(() => [
                       createTextVNode("Home")
@@ -13391,12 +13393,12 @@ const _sfc_main = {
                     _: 1
                   })
                 ]),
-                _hoisted_11
+                _hoisted_12
               ])
             ]),
             createVNode(_component_router_view),
-            _hoisted_15,
-            _hoisted_16
+            _hoisted_16,
+            _hoisted_17
           ])
         ])
       ], 64);
