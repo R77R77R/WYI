db268f9de9d77d83fd706343e7b9795f1076cdab
R <siduochen@hotmail.com>
2026-04-11 10:58:39 +0800

    auto-deploy

--- WYI.BizLogics/Init.fs
+++ WYI.BizLogics/Init.fs
@@ -159,7 +159,7 @@ let init (runtime:Runtime) =
                 | Some v -> 
                     runtime.data.providers[v.ID] <- v
                     checkBind ucat v
-                | None -> ()            
+                | None -> halt runtime.output "" ""        
 
         "DELETE FROM public.kernel_utilcat" |> tx
         "DELETE FROM public.kernel_utilprovider" |> tx
@@ -210,53 +210,53 @@ let init (runtime:Runtime) =
     Elevator Maintenance Contracts | Otis
     CO2 (for Restaurants) | (Not specified)
     Monthly Recurring Bills | (Miscellaneous)    """.Split Util.Text.lf
-        |> Array.map(fun s -> 
-            "Line214: " + s |> runtime.output
-            s.Trim())
+        |> Array.map(fun s -> s.Trim())
         |> Array.filter(fun s -> s.Contains "|")
         |> Array.map(fun s -> 
             let ss = s.Split "|"
             let cat = ss[0].Trim()
             let provider = ss[1].Trim()
-
-            "Line222: " + provider |> runtime.output
-
             cat,provider)
         |> Array.filter(fun (cat,provider) ->
             provider.StartsWith "(" = false && provider.EndsWith ")" = false)
         |> Array.iter lines.Add
     
-//        """
-//Internet | Spectrum | Comcast | Windstream/ Frontier/Century Link/Verizon Fios/Optimum/Cox/RCN
-//Cellphone | Verizon | AT&T | T-Mobile
-//Landlines
-//Cable TV | Spectrum | Comcast | Windstream/ Frontier/Century Link/Verizon Fios/Optimum/Cox/RCN
-//Satellite TV | Direct TV | Dish
-//Satellite Radio | Sirius XM
-//Security Monitoring | ADT | Vivint | Brinks/Slomin's
-//Payroll | ADP | Paychex
-//Credit Card Processing | Global | World Pay
-//Trash | Waste Mgmt. | Republic | GFL | Local companies
-//Pest Control | Terminix | Orkin/Plunkett's
-//Lawn Care | TruGreen
-//Subscriptions/Memberships | Adobe | Salesforce | HubSpot
-//Advertising | Billboards | Newspapers | Magazines | Yellow Pages
-//Water Delivery | Ready Refresh | Primo | Crystal Rock
-//Elevator Maintenance Contracts | Otis        """.Split Util.Text.lf
-//        |> Array.map(fun s -> s.Trim().Replace("/","|"))
-//        |> Array.filter(fun s -> s.Contains "|")
-//        |> Array.iter(fun s -> 
-//            s |> runtime.output
-//            let ss = s.Split "|"
-//            let cat = ss[0].Trim()
-//            [| 1 .. ss.Length - 1|]
-//            |> Array.iter(fun i ->
-//                let provider = ss[i].Trim()
-//                (cat,provider) |> lines.Add))
+        """
+Internet | Spectrum | Comcast | Windstream/ Frontier/Century Link/Verizon Fios/Optimum/Cox/RCN
+Cellphone | Verizon | AT&T | T-Mobile
+Landlines
+Cable TV | Spectrum | Comcast | Windstream/ Frontier/Century Link/Verizon Fios/Optimum/Cox/RCN
+Satellite TV | Direct TV | Dish
+Satellite Radio | Sirius XM
+Security Monitoring | ADT | Vivint | Brinks/Slomin's
+Payroll | ADP | Paychex
+Credit Card Processing | Global | World Pay
+Trash | Waste Mgmt. | Republic | GFL | Local companies
+Pest Control | Terminix | Orkin/Plunkett's
+Lawn Care | TruGreen
+Subscriptions/Memberships | Adobe | Salesforce | HubSpot
+Advertising | Billboards | Newspapers | Magazines | Yellow Pages
+Water Delivery | Ready Refresh | Primo | Crystal Rock
+Elevator Maintenance Contracts | Otis        """.Split Util.Text.lf
+        |> Array.map(fun s -> s.Trim().Replace("/","|"))
+        |> Array.filter(fun s -> s.Contains "|")
+        |> Array.iter(fun s -> 
+            s |> runtime.output
+            let ss = s.Split "|"
+            let cat = ss[0].Trim()
+            [| 1 .. ss.Length - 1|]
+            |> Array.iter(fun i ->
+                let provider = ss[i].Trim()
+                (cat,provider) |> lines.Add))
 
         lines.ToArray()
         |> Array.iter(fun (cat,provider) -> 
             cat + " / " + provider |> runtime.output
+
+            match getOrAddCat cat with
+            | Some ucat -> 
+                getOrAddProvider ucat provider
+            | None -> halt runtime.output "" ""
         )
     
     importUtilProviders()
